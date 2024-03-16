72ffb48f027a942ced1285b38fba9d5222f7a0ee
erikka-22 <iwasaki.pingoo@gmail.com>
2024-03-16 20:13:54 +0900

    add logout link

--- src/components/pages/MenuView.vue
+++ src/components/pages/MenuView.vue
@@ -2,4 +2,5 @@
   <p>メニュー</p>
   <p><RouterLink to="/quest1">quest1へ</RouterLink></p>
   <p><RouterLink to="/quest2">quest2へ</RouterLink></p>
+  <p><RouterLink to="/">ログアウト</RouterLink></p>
 </template>
