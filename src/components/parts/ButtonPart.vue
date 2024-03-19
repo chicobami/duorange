6e4db07d7b96b191d2703f335227f7979450eaa8
erikka-22 <iwasaki.pingoo@gmail.com>
2024-03-19 19:36:11 +0900

    add untracked file

---
+++ src/components/parts/ButtonPart.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: #C9A444;
+  font-family: 'Orbitron';
+  border: none;
+  box-shadow: 0rem 0.4rem #846B29;
+  border-radius: 0.7rem;
+  width: auto;
+  font-weight: bold;
+  padding: 0.5rem 0.8rem;
+  font-size: 3rem;
+  
+}
+
+</style>
+
