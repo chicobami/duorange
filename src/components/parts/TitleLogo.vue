c86d08c5e5536af4b826d0d4683325816396e355
Chie Fukuizumi <58246272+chicobami@users.noreply.github.com>
2024-03-20 21:40:27 +0900

    partsChangeFromKai

---
+++ src/components/parts/TitleLogo.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Cyber Key Quest</h1>
+  </template>
+  
+  <style scoped>
+  h1 {
+    color: #846B29;
+    font-family: 'PostNoBillsJaffnaExtraBold';
+    font-size: 7rem;
+    text-align: center;
+  }
+  </style>
