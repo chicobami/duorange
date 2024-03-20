5eca8a1d891be4d89f728d7ad4db91bb039a1014
erikka-22 <iwasaki.pingoo@gmail.com>
2024-03-20 12:41:17 +0900

    create MenuButton compornent

---
+++ src/components/templates/MenuButton.vue
@@ -0,0 +1,34 @@
+<script setup>
+import ButtonPart from '../parts/ButtonPart.vue';
+const props = defineProps({
+  isCompleted: Boolean,
+  number: Number,
+  title: String,
+  root: String,
+})
+
+</script>
+<template>
+  <RouterLink :to="{ name: 'quest'+props.number }">
+    <ButtonPart :class="{completed: props.isCompleted}">
+      <p class="quest-number">Quest{{ props.number }}</p>
+      <p class="quest-title">{{ props.title }}</p>
+    </ButtonPart>
+  </RouterLink>
+</template>
+
+<style scoped>
+.completed { 
+  background-color: #846B29;
+  box-shadow: 0rem 0.4rem #C9A444;
+}
+
+.quest-number {
+
+}
+
+.quest-title {
+
+}
+</style>
+
