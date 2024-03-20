5e4795b09a8fac315c5d31c617845989c9f43a72
Chie Fukuizumi <58246272+chicobami@users.noreply.github.com>
2024-03-20 21:40:46 +0900

    templatesChangeFromKai

---
+++ src/components/templates/MenuButton.vue
@@ -0,0 +1,51 @@
+<script setup>
+import ButtonPart from '../parts/ButtonPart.vue';
+const props = defineProps({
+  isCompleted: Boolean,
+  number: Number,
+  title: String,
+  root: String,
+})
+
+</script>
+<template>
+  <RouterLink :to="{ name: 'quest'+props.number }">
+    <ButtonPart :class="{completed: props.isCompleted}">
+      <div class="wrap-text">
+        <p class="quest-number">Quest{{ props.number }}</p>
+        <p class="quest-title">{{ props.title }}</p>
+      </div>
+      <div class="wrap-complete-stamp">
+        <div class="complete-stamp">
+          clear
+        </div>
+      </div>
+    </ButtonPart>
+  </RouterLink>
+</template>
+
+<style scoped>
+.completed { 
+  background-color: #846B29;
+  box-shadow: 0rem 0.4rem rgb(0 0 0 / 30%);
+}
+
+.wrap-text {
+  text-align: left;
+}
+
+.wrap-complete-stamp {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100%;
+}
+
+.quest-number {
+  font-size: 1.5rem;
+}
+
+.quest-title {
+
+}
+</style>
---
+++ src/components/templates/StartButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ButtonPart from '../parts/ButtonPart.vue';
+const props = defineProps(['isSignedUp'])
+
+</script>
+<template>
+  <ButtonPart>
+    <RouterLink to="/login" v-if="isSignedUp">GAME START</RouterLink>
+    <RouterLink to="/prologue" v-else>GAME START</RouterLink>
+  </ButtonPart>
+</template>
+
+<style scoped>
+a {
+  color: #ffffff;
+}
+</style>
