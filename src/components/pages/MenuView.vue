40c029eb680f87fde572178784437d60c46f5a4d
Chie Fukuizumi <58246272+chicobami@users.noreply.github.com>
2024-03-20 21:39:57 +0900

    pagesChangeFromKai

--- src/components/pages/MenuView.vue
+++ src/components/pages/MenuView.vue
@@ -1,6 +1,58 @@
+<script setup>
+import TitleLogo from '../parts/TitleLogo.vue';
+import MenuButton from '../templates/MenuButton.vue';
+
+const quests = [
+  {
+    number: 1,
+    title: 'アカウントってなに？',
+    isCompleted: true
+  },
+  {
+    number: 2,
+    title: 'フィッシング詐欺とは',
+    isCompleted: false
+  }
+]
+
+</script>
+
 <template>
-  <p>メニュー</p>
-  <p><RouterLink to="/quest1">quest1へ</RouterLink></p>
-  <p><RouterLink to="/quest2">quest2へ</RouterLink></p>
+  <div class="wrap-top-view">
+    <div class="white-cover">
+      <h1 class="menu-title">Quest</h1>
+      <MenuButton v-for="quest in quests"
+        :is-completed="quest.isCompleted"
+        :number="quest.number"
+        :title="quest.title"
+      />
+    </div>
+  </div>
   <p><RouterLink to="/">ログアウト</RouterLink></p>
 </template>
+
+<style scoped>
+.wrap-start-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.wrap-top-view {
+  background-image: url("../../../images/22823124.jpg");
+  background-size: cover;
+  width: 100vw;
+}
+
+.white-cover {
+
+}
+
+.menu-title {
+  font-family: 'Orbitron';
+  color: #ffffff;
+}
+
+
+</style>
