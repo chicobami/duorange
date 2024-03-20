56df27c104dbf0fc75f1b86a5b52854b8e5983d0
Chie Fukuizumi <58246272+chicobami@users.noreply.github.com>
2024-03-20 21:41:45 +0900

    config.jsChangeFromKai

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,31 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Unfonts({
+      google: {
+        preconnect: false,
+        display: 'block',
+        injectTo: 'head-prepend',
+        families: [
+          'Orbitron'
+        ]
+      },
+      custom: {
+        families: [
+          {
+            name: 'PostNoBillsJaffnaExtraBold',
+            local: 'PostNoBillsJaffnaExtraBold',
+            src: './src/assets/fonts/*.ttf' 
+          }
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
