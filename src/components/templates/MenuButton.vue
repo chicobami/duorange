c2886eddcb5f098752e00702388b35f7c871be6e
erikka-22 <iwasaki.pingoo@gmail.com>
2024-03-21 20:15:19 +0900

    メニューのボタンにdisabled状態のstyleを追加

--- src/components/templates/MenuButton.vue
+++ src/components/templates/MenuButton.vue
@@ -5,12 +5,17 @@ const props = defineProps({
   number: Number,
   title: String,
   root: String,
+  isDisabled: Boolean
 })
 
 </script>
 <template>
-  <RouterLink :to="{ name: 'quest'+props.number }">
-    <ButtonPart class="menu-button" :class="{completed: props.isCompleted}">
+  <RouterLink :to="{ name: 'quest'+props.number }" :class="{ 'link-disable': props.isDisabled }">
+    <ButtonPart 
+      class="menu-button" 
+      :class="{completed: props.isCompleted}"
+      :is-disabled="props.isDisabled"
+    >
       <div class="wrap-text">
         <p class="quest-number">Quest{{ props.number }}</p>
         <p class="quest-title">{{ props.title }}</p>
@@ -25,6 +30,10 @@ const props = defineProps({
 </template>
 
 <style scoped>
+.link-disable {
+  pointer-events: none;
+}
+
 .menu-button {
   padding-left: 2rem;
 }
@@ -53,7 +62,7 @@ const props = defineProps({
 }
 
 .complete-stamp-text {
-  font-size: 1.3rem;
+  font-size: 1rem;
 }
 
 .quest-number {
